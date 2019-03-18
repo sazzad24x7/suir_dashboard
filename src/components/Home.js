afc57037a65be74460310967c581c733c94f15f5
sazzad24x7 <sazzad24x7@gmail.com>
2019-03-18 12:10:35 +0600

    first experimant for layout done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component,Fragment } from 'react';
-import {Grid} from 'semantic-ui-react';
+import {Grid, Segment} from 'semantic-ui-react';
 import './App.css';
 
 class Home extends Component {
@@ -7,12 +7,34 @@ class Home extends Component {
     return (
       <Fragment>
       <Grid>
-        <Grid.Column width={4} style={{height:'800px', overflow:'auto', backgroundColor:'red'}}>
-          <h1>Left Coloum</h1>
+        <Grid.Column width={1} style={{height:"100vh", /*overflow:'auto',*/borderRight:"1px solid lightblue"}}>
+          <p>left sidebar</p>
         </Grid.Column>
-        <Grid.Column width={12} style={{ backgroundColor:'purple'}}>
-          <Grid.Row><h1>Menu</h1></Grid.Row>
-          <Grid.Row><h1>Menu 2</h1></Grid.Row>
+        <Grid.Column width={15} style={{ backgroundColor:'lightgray', paddingLeft:'0'}}>
+          <Grid.Row style={{height:"50px", backgroundColor:"white", borderBottom:"1px solid lightblue"}}><h1>Menu</h1></Grid.Row>
+          <Grid.Row style={{height:"50px", backgroundColor:"white",borderBottom:"1px solid lightblue"}}><h1>Menu 2</h1></Grid.Row>
+          <Grid.Row style={{padding:'10px 10px 0 10px'}}>
+             <Grid columns={4}>
+              <Grid.Column>
+                <Segment style={{minHeight:'100px'}}></Segment>
+              </Grid.Column>
+              <Grid.Column>
+              <Segment style={{minHeight:'100px'}}></Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment style={{minHeight:'100px'}}></Segment>
+              </Grid.Column>
+              <Grid.Column>
+              <Segment style={{minHeight:'100px'}}></Segment>
+              </Grid.Column>
+              <Grid.Column>
+                <Segment style={{minHeight:'100px'}}></Segment>
+              </Grid.Column>
+              <Grid.Column>
+              <Segment style={{minHeight:'100px'}}></Segment>
+              </Grid.Column>
+             </Grid>
+          </Grid.Row>
         </Grid.Column>
       </Grid>
       </Fragment>
